8683d85bf340b9c64b8f01be2d04852e6e5cdf5f
Валентин Черепянко <v.cherepjanko@cft.ru>
2020-04-19 20:09:27 +0700

    tests for complex chaining

--- Tests/Promises+ThenTests.swift
+++ Tests/Promises+ThenTests.swift
@@ -47,4 +47,35 @@ final class PromiseThenTests: XCTestCase {
 
         waitForExpectations(timeout: 0.1, handler: nil)
     }
+
+    func test_chaining() {
+
+        let expectation = self.expectation(description: "waiting for promise that not yet fulfilled")
+
+        func doSomething() -> Promise<Int> { return Promise(value: 1) }
+        func doAnotherThing(_ int: Int) -> Promise<String> { return Promise(value: String(int)) }
+        func lastThing(_ string: String) { expectation.fulfill(); XCTAssertEqual(string, "1") }
+
+        doSomething().then(doAnotherThing).then(lastThing)
+
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
+
+    func test_complex_chaining() {
+
+        let expectation = self.expectation(description: "waiting for promise that not yet fulfilled")
+
+        func number(_ counter: Int) -> Promise<Int> { .init(value: counter) }
+        func increment(_ value: Int) -> Promise<Int> { .init(value: value + 1) }
+        func sum(_ values: (Int, Int, Int)) -> Promise<Int> { .init(value: values.0 + values.1 + values.2) }
+        func confirm(_ value: Int) { expectation.fulfill(); XCTAssertEqual(value, 8) }
+
+        number(1)
+            .then(increment)
+            .zip(with: number(3), and: number(3))
+            .then(sum)
+            .then(confirm)
+
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
 }
