aecd363f134fa0d4f6406f1d85fa5914444d5f30
Валентин Черепянко <v.cherepjanko@cft.ru>
2020-04-19 10:31:20 +0700

    more error catching tests

--- Tests/Promise+CatchTests.swift
+++ Tests/Promise+CatchTests.swift
@@ -30,4 +30,38 @@ final class PromiseCatchTests: XCTestCase {
 
         waitForExpectations(timeout: 0.1, handler: nil)
     }
+
+    func test_then_error_catching() {
+
+        let expectation = self.expectation(description: "waiting for promise that already fulfilled")
+
+        func someWork() throws -> Int {
+            throw SomeError.foo
+        }
+
+        let work: Promise<Int>.Work = { fulfill, _ in
+            let result = try someWork()
+            fulfill(result)
+        }
+
+        Promise(work, on: .global(qos: .userInitiated))
+            .then(on: .main) { _ in XCTFail() }
+            .catch { _ in expectation.fulfill() }
+
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
+
+    func test_thenMap_error_catching() {
+
+        let expectation = self.expectation(description: "waiting for promise that already fulfilled")
+
+        let promise = Promise<Int>()
+        promise.fulfill(1)
+
+        promise
+            .thenMap(on: .main) { _ in throw SomeError.foo }
+            .catch { _ in expectation.fulfill() }
+
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
 }
