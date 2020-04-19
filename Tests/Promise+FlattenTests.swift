1179e594f31d622d7c6688b8a9d2ed04530fc386
Валентин Черепянко <v.cherepjanko@cft.ru>
2020-04-19 14:19:06 +0700

    test for race condition

--- Tests/Promise+FlattenTests.swift
+++ Tests/Promise+FlattenTests.swift
@@ -36,6 +36,43 @@ final class PromiseFlattenTests: XCTestCase {
 
         waitForExpectations(timeout: 1, handler: nil)
     }
+
+    func test_race_condition_with_multiple_queues() {
+        let worksCount = 100
+
+        let expectation = self.expectation(description: "waiting for promise")
+        expectation.expectedFulfillmentCount = worksCount + 1
+
+        let range = (0..<1000)
+        let unsortedArray = range.map { _ in Int.random(in: range) }
+
+        let makePromise: (Int) -> Promise<[Int]> = { number in
+            let queueLabel = "work.queue.\(number)"
+
+            let workQueue = Bool.random()
+                ? DispatchQueue(label: queueLabel, attributes: .concurrent)
+                : DispatchQueue(label: queueLabel)
+
+            let work: Promise<[Int]>.Work = { fulfill, _ in
+                fulfill(unsortedArray.sorted())
+                expectation.fulfill()
+                XCTAssertEqual(currentQueueLabel, queueLabel)
+            }
+            return Promise(work, on: workQueue)
+        }
+
+        var promises: [Promise<[Int]>] = []
+
+        promises = (1..<worksCount+1).map { makePromise($0) }
+
+        flatten(promises, on: .main).then { values in
+            values.forEach { XCTAssertEqual($0, values.first!) }
+            expectation.fulfill()
+            XCTAssertEqual(currentQueueLabel, "com.apple.main-thread")
+        }
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
 }
 
 private func evaluate(_ problemNumber: Int, _ problemBlock: () -> Void)
